c64ff2b735f2bf8a5ffc9e3acf7385c33f9fa030
Gina Lee <ginalee1114@gmail.com>
2018-08-04 05:16:55 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -291,7 +291,7 @@ def long_name_steals_a_ton?
         most_steals = team_data[:players][player_name][:steals]
       end 
     end 
-    binding.pry
+    debugger
     if long_name_steals >= most_steals
       return true
     end 
